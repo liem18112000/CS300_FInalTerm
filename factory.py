9787652f0739262b07e26150210521ed9d762317
Liêm <1859027@itec.hcmus.edu.vn>
2021-03-27 12:25:38 +0700

    Update factory.py

--- factory.py
+++ factory.py
@@ -110,3 +110,46 @@ class ModelFactory(object):
 
         model.summary()
         return model
+
+    def createMiniVGGModel(self):
+        model = Base_Model([
+            Input(shape=(28, 28, 1)),
+            UpSampling3D(size=(1, 1, 3)),
+
+            Conv2D(64, (3, 3), activation='relu',padding="same", input_shape=(28, 28, 3)),
+            BatchNormalization(axis=1),
+            Conv2D(64, (3, 3), activation='relu', padding="same"),
+            BatchNormalization(axis=1),
+            MaxPooling2D((2, 2)),
+            Dropout(0.25),
+
+            Conv2D(128, (3, 3), activation='relu', padding="same"),
+            BatchNormalization(axis=1),
+            Conv2D(128, (3, 3), activation='relu', padding="same"),
+            BatchNormalization(axis=1),
+            MaxPooling2D((2, 2)),
+            Dropout(0.25),
+
+            Conv2D(256, (3, 3), activation='relu', padding="same"),
+            BatchNormalization(axis=1),
+            Conv2D(256, (3, 3), activation='relu', padding="same"),         
+            BatchNormalization(axis=1),
+            MaxPooling2D((2, 2)),
+            Dropout(0.25),
+
+            Conv2D(512, (3, 3), activation='relu', padding="same"),
+            BatchNormalization(axis=1),
+            Conv2D(512, (3, 3), activation='relu', padding="same"),
+            BatchNormalization(axis=1),
+            MaxPooling2D((2, 2)),
+            Dropout(0.25),
+
+            Flatten(),
+            Dense(512, activation='relu'),
+            BatchNormalization(axis=1),
+            
+            Dense(10, activation='softmax')
+        ])
+
+        model.summary()
+        return model
