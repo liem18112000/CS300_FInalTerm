b41069beff840ff4cc2e6f97bfe897df782e9d78
Liêm <1859027@itec.hcmus.edu.vn>
2021-03-28 20:12:05 +0700

    Update factory.py

--- factory.py
+++ factory.py
@@ -91,10 +91,12 @@ class ModelFactory(object):
         model.summary()
         return model
 
-    def createPretrainModel(self, pretrain_model, trainable = False, reszie_rate=2):
+    def createPretrainModel(self, pretrain, trainable = False, reszie_rate=2):
+
+        pretrain_model, pretrain_input = pretrain
 
         core = pretrain_model(
-            include_top=False, weights='imagenet', input_shape=(28 * reszie_rate, 28 * reszie_rate, 3)
+            include_top=False, weights='imagenet', input_tensor=x
         )
 
         for layer in core.layers:
@@ -106,7 +108,8 @@ class ModelFactory(object):
             core,
             Dropout(0.5),
             Flatten(),
-            Dense(512, activation='relu'),
+            Dense(4096, activation='relu'),
+            Dense(4096, activation='relu'),
             Dense(10, activation='softmax'),
         ])
 
