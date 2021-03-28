6d3a842a594e0c19270ed08d6d12b7ec125841b9
Liêm <1859027@itec.hcmus.edu.vn>
2021-03-28 20:18:24 +0700

    Update factory.py

--- factory.py
+++ factory.py
@@ -91,15 +91,10 @@ class ModelFactory(object):
         model.summary()
         return model
 
-    def createPretrainModel(self, pretrain, trainable = False, reszie_rate=2):
+    def createPretrainModel(self, pretrain_model, trainable = False, reszie_rate=2):
 
-        pretrain_model, pretrain_input = pretrain
-
-        i = tf.keras.layers.Input((28 * reszie_rate, 28 * reszie_rate, 3), dtype=tf.uint8)
-        x = tf.cast(i, tf.float32)
-        x = pretrain_input(x)
         core = pretrain_model(
-            include_top=False, weights='imagenet', input_tensor=x
+            include_top=False, weights='imagenet', input_shape=(28 * reszie_rate, 28 * reszie_rate, 3)
         )
 
         for layer in core.layers:
